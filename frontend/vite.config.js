d3e18a26a302c37fc89743dd8dacec18704e4108
Mohit <mohitkhatri4272@gmail.com>
2023-08-27 02:56:49 +0530

    updated the server url backend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
       port: 3000,
       proxy: {
          '/api': {
-            target: 'http://localhost:8000',
+            target: 'https://task-mern-be.vercel.app/',
             changeOrigin: true,
          },
       },
