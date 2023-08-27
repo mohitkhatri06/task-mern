e51a4b44650d11b9f145431a23f5b77a618bfa3b
Mohit <mohitkhatri4272@gmail.com>
2023-08-27 15:34:50 +0530

    edited

--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -1,5 +1,5 @@
 import { apiSlice } from './apiSlice';
-const USERS_URL = 'https://task-mern-be.vercel.app/api/users';
+const USERS_URL = '/api/users';
 
 export const userApiSlice = apiSlice.injectEndpoints({
    endpoints: (builder) => ({
