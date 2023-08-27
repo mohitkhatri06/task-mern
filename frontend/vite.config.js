b30372e5341d5dee5c1140c5680b8312b4cce122
Mohit <mohitkhatri4272@gmail.com>
2023-08-27 15:25:57 +0530

    cros error solved

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,8 +5,12 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
    plugins: [react()],
    server: {
+      port: 3000,
       proxy: {
-         '/api': 'https://task-mern-be.vercel.app',
+         '/api': {
+            target: 'https://task-mern-be.vercel.app',
+            changeOrigin: true,
+         },
       },
    },
 });
